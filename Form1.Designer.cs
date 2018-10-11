6cc88ee692e57ffac84d2e2baa87d4f9f99be1ae
Your Name <you@example.com>
2018-10-10 22:11:24 -0300

    Reflection

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valor1 = new System.Windows.Forms.TextBox();
+            this.valor2 = new System.Windows.Forms.TextBox();
+            this.Somar = new System.Windows.Forms.Button();
+            this.Subtrair = new System.Windows.Forms.Button();
+            this.Multiplicar = new System.Windows.Forms.Button();
+            this.Dividir = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // valor1
+            // 
+            this.valor1.Location = new System.Drawing.Point(12, 12);
+            this.valor1.Name = "valor1";
+            this.valor1.Size = new System.Drawing.Size(100, 22);
+            this.valor1.TabIndex = 0;
+            // 
+            // valor2
+            // 
+            this.valor2.Location = new System.Drawing.Point(230, 12);
+            this.valor2.Name = "valor2";
+            this.valor2.Size = new System.Drawing.Size(100, 22);
+            this.valor2.TabIndex = 1;
+            // 
+            // Somar
+            // 
+            this.Somar.Location = new System.Drawing.Point(12, 82);
+            this.Somar.Name = "Somar";
+            this.Somar.Size = new System.Drawing.Size(75, 23);
+            this.Somar.TabIndex = 2;
+            this.Somar.Text = "+";
+            this.Somar.UseVisualStyleBackColor = true;
+            this.Somar.Click += new System.EventHandler(this.Somar_Click);
+            // 
+            // Subtrair
+            // 
+            this.Subtrair.Location = new System.Drawing.Point(93, 82);
+            this.Subtrair.Name = "Subtrair";
+            this.Subtrair.Size = new System.Drawing.Size(75, 23);
+            this.Subtrair.TabIndex = 3;
+            this.Subtrair.Text = "-";
+            this.Subtrair.UseVisualStyleBackColor = true;
+            this.Subtrair.Click += new System.EventHandler(this.Subtrair_Click);
+            // 
+            // Multiplicar
+            // 
+            this.Multiplicar.Location = new System.Drawing.Point(174, 82);
+            this.Multiplicar.Name = "Multiplicar";
+            this.Multiplicar.Size = new System.Drawing.Size(75, 23);
+            this.Multiplicar.TabIndex = 4;
+            this.Multiplicar.Text = "*";
+            this.Multiplicar.UseVisualStyleBackColor = true;
+            this.Multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
+            // 
+            // Dividir
+            // 
+            this.Dividir.Location = new System.Drawing.Point(255, 82);
+            this.Dividir.Name = "Dividir";
+            this.Dividir.Size = new System.Drawing.Size(75, 23);
+            this.Dividir.TabIndex = 5;
+            this.Dividir.Text = "/";
+            this.Dividir.UseVisualStyleBackColor = true;
+            this.Dividir.Click += new System.EventHandler(this.Dividir_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(118, 132);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(100, 22);
+            this.resultado.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 166);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.Dividir);
+            this.Controls.Add(this.Multiplicar);
+            this.Controls.Add(this.Subtrair);
+            this.Controls.Add(this.Somar);
+            this.Controls.Add(this.valor2);
+            this.Controls.Add(this.valor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox valor1;
+        private System.Windows.Forms.TextBox valor2;
+        private System.Windows.Forms.Button Somar;
+        private System.Windows.Forms.Button Subtrair;
+        private System.Windows.Forms.Button Multiplicar;
+        private System.Windows.Forms.Button Dividir;
+        private System.Windows.Forms.TextBox resultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+       static Assembly testeAssembly = Assembly.LoadFile(@"C:\Users\mathe\source\repos\WindowsFormsApp5\ClassLibrary1\obj\Debug\netstandard2.0\ClassLibrary1.dll");
+       static Type classe = testeAssembly.GetType("ClassLibrary1.Class1");
+        object ClasseInstance = Activator.CreateInstance(classe);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+   
+            
+        }
+
+        private void Somar_Click(object sender, EventArgs e)
+        {
+            
+
+
+            object ClasseInstance = Activator.CreateInstance(classe);
+            var somaa = classe.InvokeMember("Somar", BindingFlags.InvokeMethod
+                | BindingFlags.Public
+                | BindingFlags.Instance, null, ClasseInstance,
+                new object[] {Convert.ToInt32(valor1.Text), Convert.ToInt32(valor2.Text) });
+            resultado.Text = somaa.ToString();
+            
+        }
+
+        private void Subtrair_Click(object sender, EventArgs e)
+        {
+            object ClasseInstance = Activator.CreateInstance(classe);
+            var subtrairr= classe.InvokeMember("Subtrair", BindingFlags.InvokeMethod
+                | BindingFlags.Public
+                | BindingFlags.Instance, null, ClasseInstance,
+                new object[] { Convert.ToInt32(valor1.Text), Convert.ToInt32(valor2.Text) });
+            resultado.Text = subtrairr.ToString();
+        }
+
+        private void Multiplicar_Click(object sender, EventArgs e)
+        {
+            object ClasseInstance = Activator.CreateInstance(classe);
+            var multiplicarr = classe.InvokeMember("Multiplicar", BindingFlags.InvokeMethod
+                | BindingFlags.Public
+                | BindingFlags.Instance, null, ClasseInstance,
+                new object[] { Convert.ToInt32(valor1.Text), Convert.ToInt32(valor2.Text) });
+            resultado.Text = multiplicarr.ToString();
+        }
+
+        private void Dividir_Click(object sender, EventArgs e)
+        {
+            object ClasseInstance = Activator.CreateInstance(classe);
+            var dividirr = classe.InvokeMember("Dividir", BindingFlags.InvokeMethod
+                | BindingFlags.Public
+                | BindingFlags.Instance, null, ClasseInstance,
+                new object[] { Convert.ToInt32(valor1.Text), Convert.ToInt32(valor2.Text) });
+            resultado.Text = dividirr.ToString();
+        }
+    }
+}
